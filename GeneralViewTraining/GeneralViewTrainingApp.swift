1773f63982ea3de8624ca5bafcd209d8289c4cfb
Jumman55 <jumman.1948848@studenti.uniroma1.it>
2021-12-29 21:01:27 +0100

    Initial Commit

---
+++ GeneralViewTraining/GeneralViewTrainingApp.swift
@@ -0,0 +1,17 @@
+//
+//  GeneralViewTrainingApp.swift
+//  GeneralViewTraining
+//
+//  Created by Jumman Hossen on 29/12/21.
+//
+
+import SwiftUI
+
+@main
+struct GeneralViewTrainingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
